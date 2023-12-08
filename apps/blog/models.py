f11f12dca07c7a39afc4f3ca0df3ec42ae21f28e
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-12-08 15:04:34 +0500

    added ckeditor

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class ArticleCategory(models.Model):
@@ -12,7 +12,7 @@ class Article(models.Model):
     title = models.CharField(max_length=100, null=True, blank=True)
     category = models.ForeignKey(ArticleCategory, on_delete=models.CASCADE)
     subtitle = models.CharField(max_length=100, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    description = RichTextField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
